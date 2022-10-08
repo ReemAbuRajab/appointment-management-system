044de5fee83419b3199dcf1b650fb0dfa7a45bc6
MK <MK@DESKTOP-Q03SV6H>
2022-10-08 15:21:48 +0300

    AcceptanceTest

--- hospital/src/test/java/hospital/AcceptanceTest.java
+++ hospital/src/test/java/hospital/AcceptanceTest.java
@@ -1,4 +1,18 @@
 package hospital;
+import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import io.cucumber.junit.CucumberOptions.SnippetType;
+
+@RunWith(Cucumber.class)
+
+@CucumberOptions(
+		tags= "RegressionTesting",
+		features = "use_cases",
+		plugin = {"html: target/cucmber.html"},
+		monochrome = true,
+		snippets = SnippetType.CAMELCASE,
+		glue = {"facebook"})
 
 public class AcceptanceTest {
 
